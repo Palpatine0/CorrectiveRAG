1ed6f9460d7d9235f2213377e6c13eca64605a07
Percival <emperorsheevpalpatine0@gmail.com>
2024-06-22 13:23:33 +0800

    feat: add relevance scoring
    - Added a prompt and chain to assess the relevance of retrieved documents using `ChatMistralAI`.

--- corrective-rag/app/chain.py
+++ corrective-rag/app/chain.py
@@ -3,12 +3,16 @@ from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import Chroma
 from langchain_nomic.embeddings import NomicEmbeddings
 
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_community.chat_models import ChatOllama
+from langchain_mistralai.chat_models import ChatMistralAI
+
 from dotenv import load_dotenv
 from langchain.callbacks.tracers.langchain import wait_for_all_tracers
 
 load_dotenv()
 wait_for_all_tracers()
-
 
 # Load
 url = "https://lilianweng.github.io/posts/2023-06-23-agent/"
@@ -17,17 +21,36 @@ docs = loader.load()
 
 # Split
 text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-    chunk_size=500, chunk_overlap=100
+    chunk_size = 500, chunk_overlap = 100
 )
 all_splits = text_splitter.split_documents(docs)
 
-
 # Index
 vectorstore = Chroma.from_documents(
-    documents=all_splits,
-    collection_name="rag-chroma",
+    documents = all_splits,
+    collection_name = "rag-chroma",
     embedding = NomicEmbeddings(model = "nomic-embed-text-v1"),
 )
 retriever = vectorstore.as_retriever()
 
-print(retriever.get_relevant_documents("Agent memory"))
+prompt = PromptTemplate(
+    template = """
+    You are a grader assessing relevance of a retrieved document to a user question.  
+    Here is the retrieved document: 
+    {context}
+    Here is the user question: 
+    {question} 
+    If the document contains keywords related to the user question, grade it as relevant.
+    It does not need to be a stringent test. The goal is to filter out erroneous retrievals.
+    Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question. 
+    Provide the binary score as a JSON with a single key 'score' and no premable or explanation.
+    """,
+    input_variables = ["question", "document"],
+)
+
+chain = prompt | ChatMistralAI(model="mistral-medium", temperature=0) | JsonOutputParser()
+question = "agent memory"
+docs = retriever.get_relevant_documents(question)
+
+score = chain.invoke({"question": question, "context": docs[0].page_content})
+print(score)
